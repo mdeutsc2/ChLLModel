bce28ad77398cf3a49f972b5f5ddc78557263dca
Matthew Deutsch <87136927+mdeutsc2@users.noreply.github.com>
2024-06-21 21:53:04 -0600

    Update phase_diagram.py

--- phase_diagram.py
+++ phase_diagram.py
@@ -20,13 +20,57 @@ def init_aligned(sim):
 	sim.ny[:,:,:] = 0.0
 	sim.nz[:,:,:] = 0.0
 
-icount = 1
+"""
 for temp in np.arange(0.05,1.51,0.05):
 	for K in np.arange(0.1,1.1,0.1):
-		simename = "phasediag_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
+		simename = "phasediag_aligned_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
 		sim = chll.ChLLSim(name = simename,ni=64,nj=64,nk=64,kbt=temp,d = 0.01,KK = K,rho = 0.0)
 		init_aligned(sim)
 		sim.init()
 		sim.run(10000,50,save=True)
 		sim.plot_config()
-	
+"""	
+for temp in np.arange(0.05,1.51,0.05):
+	for K in np.arange(0.01,0.11,0.01):
+		simename = "phasediag_aligned_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
+		sim = chll.ChLLSim(name = simename,ni=64,nj=64,nk=64,kbt=temp,d = 0.01,KK = K,rho = 0.0)
+		init_aligned(sim)
+		sim.init()
+		sim.run(10000,50,save=True)
+		sim.plot_config()
+
+for temp in np.arange(0.05,1.51,0.05):
+	for K in np.arange(0.001,0.011,0.001):
+		simename = "phasediag_aligned_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
+		sim = chll.ChLLSim(name = simename,ni=64,nj=64,nk=64,kbt=temp,d = 0.01,KK = K,rho = 0.0)
+		init_aligned(sim)
+		sim.init()
+		sim.run(10000,50,save=True)
+		sim.plot_config()
+
+for temp in np.arange(0.05,1.51,0.05):
+	for K in np.arange(0.1,1.1,0.1):
+		simename = "phasediag_random_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
+		sim = chll.ChLLSim(name = simename,ni=64,nj=64,nk=64,kbt=temp,d = 0.01,KK = K,rho = 0.0)
+		init_random(sim)
+		sim.init()
+		sim.run(10000,50,save=True)
+		sim.plot_config()
+
+for temp in np.arange(0.05,1.51,0.05):
+	for K in np.arange(0.01,0.11,0.01):
+		simename = "phasediag_random_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
+		sim = chll.ChLLSim(name = simename,ni=64,nj=64,nk=64,kbt=temp,d = 0.01,KK = K,rho = 0.0)
+		init_random(sim)
+		sim.init()
+		sim.run(10000,50,save=True)
+		sim.plot_config()
+
+for temp in np.arange(0.05,1.51,0.05):
+	for K in np.arange(0.001,0.011,0.001):
+		simename = "phasediag_random_K"+str(round(K,3))+"_kbt"+str(round(temp,5))
+		sim = chll.ChLLSim(name = simename,ni=64,nj=64,nk=64,kbt=temp,d = 0.01,KK = K,rho = 0.0)
+		init_random(sim)
+		sim.init()
+		sim.run(10000,50,save=True)
+		sim.plot_config()
