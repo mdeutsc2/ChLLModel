91410b0a7fada5744f8edfb2f44539233da7849b
Matthew Deutsch <87136927+mdeutsc2@users.noreply.github.com>
2024-09-23 12:17:40 -0400

    tabs vs. spaces

--- chll/chll.py
+++ chll/chll.py
@@ -82,23 +82,23 @@ class ChLLSim:
 			self.sl1 = np.zeros(int((self.ni*self.nj*self.nk)/2),dtype=np.int32,order='F')
 
 	def init_rand_s(self):
-	    for i in range(self.ni):
+		for i in range(self.ni):
 			for j in range(self.nj):
-		    	for k in range(self.nk):
+				for k in range(self.nk):
 					self.s[i,j,k] = 1
 					if np.random.rand() < 0.5:
-			    		self.s[i,j,k] = -1
-	    if ((np.mean(np.sqrt(self.nx*self.nx+self.ny*self.ny+self.nz*self.nz))) == 0.0):
+						self.s[i,j,k] = -1
+		if ((np.mean(np.sqrt(self.nx*self.nx+self.ny*self.ny+self.nz*self.nz))) == 0.0):
 			print("> Nx,Ny,Nz not initialized, setting nx[:,:,:] = 1.0")
 			for i in range(self.ni):
-		    	for j in range(self.nj):
+				for j in range(self.nj):
 					for k in range(self.nk):
-			    		self.nx[i,j,k] = 1.0
-			    		self.ny[i,j,k] = 0.0
-			    		self.nz[i,j,k] = 0.0
-	    ftn_init(self.nx,self.ny,self.nz,self.s,self.dope.self.sl1,self.sl2,
-		    ct.c_int(self.ni),ct.c_int(self.nj),ct.c_int(self.nk),ct.c_int(self.nsub))
-	    print("> Init Done!")
+						self.nx[i,j,k] = 1.0
+						self.ny[i,j,k] = 0.0
+						self.nz[i,j,k] = 0.0
+		ftn_init(self.nx,self.ny,self.nz,self.s,self.dope.self.sl1,self.sl2,
+			ct.c_int(self.ni),ct.c_int(self.nj),ct.c_int(self.nk),ct.c_int(self.nsub))
+		print("> Init Done!")
 
 	def init_with_s(self,init_s):
 		self.s[:,:,:] = init_s
